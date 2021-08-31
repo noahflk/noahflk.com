543bcc3eb8c73c3ec0815f55e6683acceab25d9c
Noah Falk <hi@noahfalk.com>
2021-08-31 23:27:00 +0200

    Update font

--- styles/theme.js
+++ styles/theme.js
@@ -2,6 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   fonts: {
+    heading: `Inter, system-ui, sans-serif`,
     body: `Inter, system-ui, sans-serif`,
   },
   config: {
