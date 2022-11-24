cfc3df8e2101c304a2bc9008ae0f88bbb92955b6
Noah <hi@noahflk.com>
2022-11-24 17:51:42 +0200

    Properly configure pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,20 @@ import tailwind from '@astrojs/tailwind';
 import prefetch from '@astrojs/prefetch';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
-
 import svelte from '@astrojs/svelte';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://noahflk.com/',
-  integrations: [
-    sitemap(),
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-    prefetch(),
-    image(),
-    mdx(),
-    svelte(),
-  ],
+  integrations: [sitemap(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), prefetch(), image(), mdx(), svelte()],
   markdown: {
     shikiConfig: {
       theme: 'poimandres',
-      wrap: true,
-    },
-  },
+      wrap: true
+    }
+  }
 });
