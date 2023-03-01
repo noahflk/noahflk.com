39bc4519061fed4143ec924f62ba5af07e8cab97
Noah <hi@noahflk.com>
2023-03-01 22:51:05 +0100

    Fix truncated URL issue

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,10 +1,3 @@
 import type { Post } from '@/types/blog';
 
-export const removeIndexFromSlug = (slug: string) => slug.slice(0, -6);
-
-// slice to remove /index from the end of the slug
-export const getBlogUrl = (post: Post) => `/blog/${removeIndexFromSlug(post.slug)}`;
-
-export const getSlugFromPath = (path: string): string => {
-  return path.split('/').at(-2) ?? '';
-};
+export const getBlogUrl = (post: Post) => `/blog/${post.slug}`;
