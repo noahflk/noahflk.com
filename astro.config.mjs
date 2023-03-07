561ebc80eb4e4ce28d38413cc6c4f36e4100085d
Noah <hi@noahflk.com>
2023-03-07 21:16:40 +0100

    Remove trailing slash from site

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://noahflk.com/',
+  site: 'https://noahflk.com',
   integrations: [
     sitemap(),
     tailwind({
