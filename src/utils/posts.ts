ec790fde6ee8a9f2279ad130a85a92c51c3265ba
Noah <hi@noahflk.com>
2024-04-03 18:28:40 -0600

    Fix issue on home page

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -6,8 +6,6 @@ export const getSortedPosts = async (): Promise<Post[]> => {
   const posts = await getCollection('posts');
   const blog = await getCollection('blog');
 
-  console.log(posts, 'sdfdslsdjfk');
-
   const allPosts = [...posts, ...blog];
 
   return allPosts.sort((a, b) => new Date(b.data.pubDate).valueOf() - new Date(a.data.pubDate).valueOf());
